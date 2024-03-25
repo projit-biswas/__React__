b60ac7a3153910f5b8cedef211a919a4c9960be5
Projit Biswas <projit.biswas99@gmail.com>
2024-03-25 10:53:23 +0530

    todo create

--- Xebia/src/App.jsx
+++ Xebia/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Header from "./component/Homepage/Header.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState("");
+
+  const handelChange = (e) => {
+    console.log(e.target.value);
+    setData(e.target.value);
+  };
 
   return (
     <>
-      <div className="w-screen h-8 bg-orange-400">Projit</div>
+      <div className="text-center mt-10 flex justify-center flex-col">
+        <h1 className="text-center text-3xl font-bold m-9 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text">
+          A Simple TODO
+        </h1>
+        <label>
+          TODO :
+          <input
+            className="border-[#9c9ca7] hover:border-dashed  border-2 rounded-md p-2 mx-2 w-72"
+            type="text"
+            value={data}
+            placeholder="Write a TODO"
+            onChange={handelChange}
+          />
+        </label>
+        <div>{data}</div>
+      </div>
     </>
   );
 }
